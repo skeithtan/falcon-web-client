c5ad8d3b8137c74d5fb9154ffacc0077d65ddf55
Keith Tan <skeithtan@gmail.com>
2018-09-05 17:16:14 +0800

    Add basic page routing

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,78 @@
 import { inject, observer } from "mobx-react";
 import * as React from "react";
+import { RouteComponentProps, withRouter } from "react-router-dom";
+import Page, {
+    IPageSpecification,
+    PAGE_SPECIFICATION,
+} from "./models/enums/page";
 import SignInPage from "./pages/SignIn";
 import { AuthenticationState } from "./store/authentication";
 
-interface IPropsType {
+interface IRouteParameters {
+    page?: string;
+}
+
+interface IPropsType extends RouteComponentProps<IRouteParameters> {
     authentication?: AuthenticationState;
 }
 
 @inject("authentication")
 @observer
-export default class App extends React.Component<IPropsType> {
+class App extends React.Component<IPropsType> {
     public renderSignInPage() {
         return <SignInPage />;
+    }
+
+    public handlePath = () => {
+        const {
+            match: { params },
+            history,
+            authentication,
+        } = this.props;
+
+        // Non signed-in users will not be entertained
+        if (!authentication!.currentUser) {
+            return;
+        }
+
+        const page = params.page;
+
+        // If no selected page, redirect to default page
+        if (!page) {
+            const defaultPage = authentication!.currentUser!.getDefaultPage();
+            const defaultPagePath = PAGE_SPECIFICATION[defaultPage].path;
+            history.push(defaultPagePath);
+            return;
+        }
+
+        const pageSpecification = Object.values(PAGE_SPECIFICATION).find(
+            (ps: IPageSpecification) => ps.path === page
+        );
+
+        // If page could not be found, go to 404 page
+        const notFound = !pageSpecification;
+        const notAllowed =
+            pageSpecification &&
+            !authentication!
+                .currentUser!.getVisitablePages()
+                .includes(pageSpecification.page);
+
+        if (notFound || notAllowed) {
+            history.push(PAGE_SPECIFICATION[Page.NotFound].path);
+            return;
+        }
+    };
+
+    //
+    // ─── Lifecycle Functions ───────────────────────────────────────────────────────────────────────────
+    //
+
+    public componentDidMount() {
+        this.handlePath();
+    }
+
+    public componentDidUpdate(prevProps: IPropsType) {
+        this.handlePath();
     }
 
     public render() {
@@ -24,3 +85,5 @@ export default class App extends React.Component<IPropsType> {
         return this.renderSignInPage();
     }
 }
+
+export default withRouter<IPropsType>(App);
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { Provider } from "mobx-react";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import App from "./app";
 import { initializeApp } from "./app/controllers/initialize_app";
 import store from "./app/store";
@@ -10,7 +11,9 @@ import registerServiceWorker from "./registerServiceWorker";
 const app = (
     <CssBaseline>
         <Provider {...store}>
-            <App />
+            <BrowserRouter>
+                <Route path="/:page?" component={App} />
+            </BrowserRouter>
         </Provider>
     </CssBaseline>
 );
