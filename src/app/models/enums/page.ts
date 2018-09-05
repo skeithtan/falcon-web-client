1a5fe0b3d27bc994b5dcbf5b8c049042fb43b6d2
Keith Tan <skeithtan@gmail.com>
2018-09-05 12:50:59 +0800

    Add page enum

---
+++ src/app/models/enums/page.ts
@@ -0,0 +1,22 @@
+enum Page {
+    //
+    // ─── Administrative ───────────────────────────────────────────────────────────────────────────
+    //
+    FacultyProfiles,
+    Subjects,
+    FacultyLoading,
+    Users,
+
+    //
+    // ─── Faculty Members ───────────────────────────────────────────────────────────────────────────
+    //
+    MyProfile,
+    MySchedule,
+
+    //
+    // ─── Universal ───────────────────────────────────────────────────────────────────────────
+    //
+    NotFound,
+}
+
+export default Page;
