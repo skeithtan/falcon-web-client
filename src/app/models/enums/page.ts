1f71a4cae8726ad853d472e06059c486c2f4b478
Keith Tan <skeithtan@gmail.com>
2018-09-05 17:16:03 +0800

    Add page specifications to Page enum

--- src/app/models/enums/page.ts
+++ src/app/models/enums/page.ts
@@ -1,3 +1,5 @@
+import { Component } from "react";
+
 enum Page {
     //
     // ─── Administrative ───────────────────────────────────────────────────────────────────────────
@@ -18,5 +20,62 @@ enum Page {
     //
     NotFound,
 }
+
+export interface IPageSpecification {
+    page: Page;
+    path: string;
+    component?: Component;
+    name: string;
+    pathParameters?: string;
+}
+
+export const PAGE_SPECIFICATION: { [key: number]: IPageSpecification } = {
+    [Page.FacultyProfiles]: {
+        page: Page.FacultyProfiles,
+        path: "faculty-profiles",
+        component: undefined,
+        name: "Faculty Profiles",
+        pathParameters: "/:facultyId?/:activeTab?",
+    },
+    [Page.Subjects]: {
+        page: Page.Subjects,
+        path: "subjects",
+        component: undefined,
+        name: "Subjects",
+        pathParameters: "/:subjectId?",
+    },
+    [Page.FacultyLoading]: {
+        page: Page.FacultyLoading,
+        path: "faculty-loading",
+        component: undefined,
+        name: "Faculty Loading",
+        pathParameters: "/:termId?/:meetingDays?",
+    },
+    [Page.Users]: {
+        page: Page.Users,
+        path: "users",
+        component: undefined,
+        name: "Users",
+    },
+    [Page.MyProfile]: {
+        page: Page.MyProfile,
+        path: "my-profile",
+        component: undefined,
+        name: "My Profile",
+    },
+    [Page.MySchedule]: {
+        page: Page.MySchedule,
+        path: "my-schedule",
+        component: undefined,
+        name: "My Schedue",
+        pathParameters: "/:termId?",
+    },
+    [Page.NotFound]: {
+        page: Page.NotFound,
+        path: "404",
+        component: undefined,
+        name: "Page Not Found",
+    },
+};
 
 export default Page;
