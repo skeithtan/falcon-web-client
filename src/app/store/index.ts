6ad586b3fbb2bbb0194e1ff702832bf14cf1ece3
Keith Tan <skeithtan@gmail.com>
2018-09-28 20:39:53 +0800

    Add store to window in development

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -11,4 +11,15 @@ const rootStore: IRootStore = {
     facultyProfiles,
 };
 
+// Put rootStore as window variable when debugging
+if (
+    process &&
+    process.env &&
+    process.env.NODE_ENV &&
+    process.env.NODE_ENV === "development"
+) {
+    // tslint:disable-next-line
+    window["store"] = rootStore;
+}
+
 export default rootStore;
