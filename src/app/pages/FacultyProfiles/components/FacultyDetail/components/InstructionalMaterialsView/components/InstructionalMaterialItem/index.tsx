8292dd0498c03062a043bc3a1ce3065e811b807e
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-03 22:12:26 +0800

    Create instructional material view item

---
+++ src/app/pages/FacultyProfiles/components/FacultyDetail/components/InstructionalMaterialsView/components/InstructionalMaterialItem/index.tsx
@@ -0,0 +1,66 @@
+import IconButton from "@material-ui/core/IconButton";
+import TableCell from "@material-ui/core/TableCell";
+import TableRow from "@material-ui/core/TableRow";
+import Delete from "@material-ui/icons/Delete";
+import * as React from "react";
+import ProgramChip from "../../../../../../../../components/reusable/ProgramChip";
+import InstructionalMaterial from "../../../../../../../../models/entities/instructional_material";
+import InstructionalMaterialAudience, {
+    InstructionalMaterialAudienceReadable,
+} from "../../../../../../../../models/enums/instructional_material_audience";
+import InstructionalMaterialMedium, {
+    InstructionalMaterialMediumReadable,
+} from "../../../../../../../../models/enums/instructional_material_medium";
+import Program, {
+    ProgramReadable,
+} from "../../../../../../../../models/enums/program";
+
+interface IPropsType {
+    instructionalMaterial: InstructionalMaterial;
+}
+
+export default class InstructionalMaterialItem extends React.Component<
+    IPropsType
+> {
+    public render() {
+        const { instructionalMaterial } = this.props;
+        return (
+            <TableRow>
+                <TableCell>{instructionalMaterial.title}</TableCell>
+                <TableCell>
+                    {
+                        InstructionalMaterialMediumReadable.get(
+                            instructionalMaterial.medium
+                        ) as InstructionalMaterialMedium
+                    }
+                </TableCell>
+                <TableCell>
+                    {
+                        InstructionalMaterialAudienceReadable.get(
+                            instructionalMaterial.audience
+                        ) as InstructionalMaterialAudience
+                    }
+                </TableCell>
+                <TableCell>{instructionalMaterial.usageYear}</TableCell>
+                <TableCell>{instructionalMaterial.level}</TableCell>
+                <TableCell>
+                    {instructionalMaterial.associatedPrograms.map(ap => {
+                        return (
+                            <ProgramChip
+                                key={ap}
+                                program={ProgramReadable.get(ap) as Program}
+                            />
+                        );
+                    })}
+                </TableCell>
+                <TableCell>
+                    <React.Fragment>
+                        <IconButton>
+                            <Delete />
+                        </IconButton>
+                    </React.Fragment>
+                </TableCell>
+            </TableRow>
+        );
+    }
+}
