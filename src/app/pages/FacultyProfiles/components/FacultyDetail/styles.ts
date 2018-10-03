7420680af21a440d76614ef53bbc153684ed10c5
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-03 15:06:24 +0800

    Style faculty detail

---
+++ src/app/pages/FacultyProfiles/components/FacultyDetail/styles.ts
@@ -0,0 +1,10 @@
+import { StyleRules, Theme } from "@material-ui/core/styles";
+
+export default (theme: Theme): StyleRules => ({
+    detail: {
+        padding: theme.spacing.unit * 5,
+    },
+    item: {
+        width: "100%",
+    },
+});
