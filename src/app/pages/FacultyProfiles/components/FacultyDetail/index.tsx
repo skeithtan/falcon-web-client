6022b28c8206187625bc0fdfc18e45722ab112ec
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-03 15:06:45 +0800

    Add base components for faculty detail

--- src/app/pages/FacultyProfiles/components/FacultyDetail/index.tsx
+++ src/app/pages/FacultyProfiles/components/FacultyDetail/index.tsx
@@ -1,24 +1,72 @@
+import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core/styles";
 import { inject, observer } from "mobx-react";
 import * as React from "react";
+import IStyleClasses from "../../../../interfaces/style_classes";
 import { FacultyProfilesState } from "../../../../store/faculty_profiles";
+import BasicInformationView from "./components/BasicInformationView";
+import FacultySubdocumentCard from "./components/FacultySubdocumentCard";
+import styles from "./styles";
 
 interface IPropsType {
+    classes: IStyleClasses;
     facultyProfiles?: FacultyProfilesState;
 }
 
 @inject("facultyProfiles")
 @observer
-export default class FacultyDetail extends React.Component<IPropsType> {
+class FacultyDetail extends React.Component<IPropsType> {
+    public temporaryAddClick() {
+        global.console.log("Cool! You wanna add something!");
+    }
+
     public render() {
-        const { facultyProfiles } = this.props;
+        const { classes, facultyProfiles } = this.props;
         const { activeFacultyMember } = facultyProfiles!;
 
         return (
             <div>
+                {activeFacultyMember === undefined && (
+                    <Grid
+                        container
+                        direction="row"
+                        justify="flex-start"
+                        alignItems="center"
+                    >
+                        <Grid item>Hey select someone</Grid>
+                    </Grid>
+                )}
+
                 {activeFacultyMember !== undefined && (
-                    <div>{activeFacultyMember.pnuId}</div>
+                    <Grid
+                        container
+                        direction="column"
+                        justify="flex-start"
+                        alignItems="center"
+                        className={classes.detail}
+                        spacing={24}
+                    >
+                        <Grid item className={classes.item}>
+                            <BasicInformationView
+                                facultyMember={activeFacultyMember}
+                                canUpdate
+                            />
+                        </Grid>
+                        <Grid item className={classes.item}>
+                            <FacultySubdocumentCard
+                                empty={false}
+                                name="test"
+                                onAddClick={this.temporaryAddClick}
+                                canAdd={true}
+                            >
+                                <div>Something</div>
+                            </FacultySubdocumentCard>
+                        </Grid>
+                    </Grid>
                 )}
             </div>
         );
     }
 }
+
+export default withStyles(styles)(FacultyDetail);
