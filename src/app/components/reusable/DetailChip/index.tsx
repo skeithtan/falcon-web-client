fb2a4399e5e579d07395ecac2c2c8ccb8b65e387
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-09 19:37:59 +0800

    Rename program chip to detail chip

--- src/app/components/reusable/DetailChip/index.tsx
+++ src/app/components/reusable/DetailChip/index.tsx
@@ -5,15 +5,15 @@ import IStyleClasses from "../../../interfaces/style_classes";
 import styles from "./styles";
 
 interface IPropsType {
-    program: string;
+    detail: string;
     classes: IStyleClasses;
 }
 
-class ProgramChip extends React.Component<IPropsType> {
+class DetailChip extends React.Component<IPropsType> {
     public render() {
-        const { program, classes } = this.props;
-        return <Chip label={program} className={classes.chip} />;
+        const { detail, classes } = this.props;
+        return <Chip label={detail} className={classes.chip} />;
     }
 }
 
-export default withStyles(styles)(ProgramChip);
+export default withStyles(styles)(DetailChip);
