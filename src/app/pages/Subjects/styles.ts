f9758f695194e62c23796005e40300c4cc75031b
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-09 19:41:53 +0800

    Style subjects page

---
+++ src/app/pages/Subjects/styles.ts
@@ -0,0 +1,16 @@
+import { StyleRules } from "@material-ui/core/styles";
+
+export default {
+    root: {
+        height: "100%",
+    },
+    list: {
+        minWidth: 280,
+        overflowY: "scroll",
+    },
+    detail: {
+        width: "100%",
+        overflowY: "scroll",
+        overflowX: "hidden",
+    },
+} as StyleRules;
