3f6324a125c9010fabc927f24bc640bbaf7e2ab9
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-10 11:02:30 +0800

    Change chip margin

--- src/app/components/reusable/DetailChip/styles.ts
+++ src/app/components/reusable/DetailChip/styles.ts
@@ -2,6 +2,6 @@ import { StyleRules, Theme } from "@material-ui/core/styles";
 
 export default (theme: Theme): StyleRules => ({
     chip: {
-        margin: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
     },
 });
