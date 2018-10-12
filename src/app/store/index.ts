a3a6540a77766e5086c22d33845816f6d292a5ca
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-12 15:27:39 +0800

    Remove my profile from root store

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,20 +1,17 @@
 import authentication, { AuthenticationState } from "./authentication";
 import facultyProfiles, { FacultyProfilesState } from "./faculty_profiles";
-import myProfile, { MyProfileState } from "./my_profile";
 import subjects, { SubjectsState } from "./subjects";
 
 interface IRootStore {
     authentication: AuthenticationState;
     facultyProfiles: FacultyProfilesState;
     subjects: SubjectsState;
-    myProfile: MyProfileState;
 }
 
 const rootStore: IRootStore = {
     authentication,
     facultyProfiles,
     subjects,
-    myProfile,
 };
 
 // Put rootStore as window variable when debugging
