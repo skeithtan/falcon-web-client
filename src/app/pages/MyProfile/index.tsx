d21653bf8b389a5e963087b5e6eaab5d86fef4a7
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-12 15:27:22 +0800

    Reduce my profile view
    Now reuses faculty detail

--- src/app/pages/MyProfile/index.tsx
+++ src/app/pages/MyProfile/index.tsx
@@ -1,32 +1,24 @@
-import Grid from "@material-ui/core/Grid";
 import { withStyles } from "@material-ui/core/styles";
 import { inject, observer } from "mobx-react";
 import * as React from "react";
 import StateWrapper from "src/app/components/reusable/StateWrapper";
-import MyProfileController from "src/app/controllers/my_profile";
 import IStyleClasses from "src/app/interfaces/style_classes";
-import { MyProfileState } from "src/app/store/my_profile";
+import { FacultyProfilesState } from "src/app/store/faculty_profiles";
 import FacultyProfilesController from "../../controllers/faculty_profiles";
-import BasicInformationView from "../FacultyProfiles/components/FacultyDetail/components/BasicInformationView";
-import DegreesView from "../FacultyProfiles/components/FacultyDetail/components/DegreesView";
-import ExtensionWorksView from "../FacultyProfiles/components/FacultyDetail/components/ExtensionWorksView";
-import FacultySubdocumentCard from "../FacultyProfiles/components/FacultyDetail/components/FacultySubdocumentCard";
-import InstructionalMaterialsView from "../FacultyProfiles/components/FacultyDetail/components/InstructionalMaterialsView";
-import PresentationsView from "../FacultyProfiles/components/FacultyDetail/components/PresentationsView";
-import RecognitionsView from "../FacultyProfiles/components/FacultyDetail/components/RecognitionsView";
+import FacultyDetail from "../FacultyProfiles/components/FacultyDetail";
 import styles from "./styles";
 
 interface IPropsType {
     classes: IStyleClasses;
-    myProfile?: MyProfileState;
+    facultyProfiles: FacultyProfilesState;
 }
 
-@inject("myProfile")
+@inject("facultyProfiles")
 @observer
 class MyProfile extends React.Component<IPropsType> {
     public componentDidMount() {
         document.title = "My Profile - Falcon";
-        MyProfileController.getCurrentFaculty();
+        FacultyProfilesController.getCurrentFaculty();
     }
 
     public addDegreeFormToggle = (shouldShow: boolean) => () => {
@@ -52,99 +44,15 @@ class MyProfile extends React.Component<IPropsType> {
     };
 
     public render() {
-        const { classes, myProfile } = this.props;
-        const { profile } = myProfile!;
+        const { facultyProfiles, classes } = this.props;
         return (
-            <StateWrapper fetchableState={myProfile!.fetchStatus} disableFlex>
+            <StateWrapper
+                fetchableState={facultyProfiles!.fetchStatus}
+                disableFlex
+            >
                 {() => (
                     <div className={classes.root}>
-                        <Grid
-                            container
-                            className={classes.myProfileGrid}
-                            direction="column"
-                            justify="center"
-                            alignItems="stretch"
-                            wrap="nowrap"
-                            spacing={24}
-                        >
-                            <Grid item className={classes.item}>
-                                <BasicInformationView
-                                    facultyMember={profile}
-                                    canUpdate
-                                />
-                            </Grid>
-                            <Grid item className={classes.item}>
-                                <FacultySubdocumentCard
-                                    empty={profile.degrees!.length === 0}
-                                    name="Degrees"
-                                    onAddClick={this.addDegreeFormToggle(true)}
-                                    canAdd
-                                >
-                                    <DegreesView degrees={profile.degrees!} />
-                                </FacultySubdocumentCard>
-                            </Grid>
-                            <Grid item className={classes.item}>
-                                <FacultySubdocumentCard
-                                    empty={profile.recognitions!.length === 0}
-                                    name="Academic Recognitions"
-                                    onAddClick={this.addRecognitionFormToggle(
-                                        true
-                                    )}
-                                    canAdd
-                                >
-                                    <RecognitionsView
-                                        recognitions={profile.recognitions!}
-                                    />
-                                </FacultySubdocumentCard>
-                            </Grid>
-                            <Grid item className={classes.item}>
-                                <FacultySubdocumentCard
-                                    empty={profile.presentations!.length === 0}
-                                    name="Presentations"
-                                    onAddClick={this.addPresentationFormToggle(
-                                        true
-                                    )}
-                                    canAdd
-                                >
-                                    <PresentationsView
-                                        presentations={profile.presentations!}
-                                    />
-                                </FacultySubdocumentCard>
-                            </Grid>
-                            <Grid item className={classes.item}>
-                                <FacultySubdocumentCard
-                                    empty={
-                                        profile.instructionalMaterials!
-                                            .length === 0
-                                    }
-                                    name="Instructional Materials"
-                                    onAddClick={this.addInstructionalMaterialFormToggle(
-                                        true
-                                    )}
-                                    canAdd
-                                >
-                                    <InstructionalMaterialsView
-                                        instructionalMaterials={
-                                            profile.instructionalMaterials!
-                                        }
-                                    />
-                                </FacultySubdocumentCard>
-                            </Grid>
-                            <Grid item className={classes.item}>
-                                <FacultySubdocumentCard
-                                    empty={profile.extensionWorks!.length === 0}
-                                    name="Extension Works"
-                                    onAddClick={this.addExtensionWorkFormToggle(
-                                        true
-                                    )}
-                                    canAdd
-                                >
-                                    <ExtensionWorksView
-                                        extensionWorks={profile.extensionWorks!}
-                                    />
-                                </FacultySubdocumentCard>
-                            </Grid>
-                        </Grid>
+                        <FacultyDetail />
                     </div>
                 )}
             </StateWrapper>
