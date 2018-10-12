8b85a1295d96a4d7ccbf63c99af15d9d1ad21fb3
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-12 15:03:07 +0800

    Add my profile to store

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,17 +1,20 @@
 import authentication, { AuthenticationState } from "./authentication";
 import facultyProfiles, { FacultyProfilesState } from "./faculty_profiles";
+import myProfile, { MyProfileState } from "./my_profile";
 import subjects, { SubjectsState } from "./subjects";
 
 interface IRootStore {
     authentication: AuthenticationState;
     facultyProfiles: FacultyProfilesState;
     subjects: SubjectsState;
+    myProfile: MyProfileState;
 }
 
 const rootStore: IRootStore = {
     authentication,
     facultyProfiles,
     subjects,
+    myProfile,
 };
 
 // Put rootStore as window variable when debugging
