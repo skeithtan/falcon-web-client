f64afbf0810bc8b5622546655fa9a457fc42dc60
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-13 20:27:58 +0800

    Create reusable print preview dialog

---
+++ src/app/components/reusable/PrintPreviewDialog/index.tsx
@@ -0,0 +1,56 @@
+import AppBar from "@material-ui/core/AppBar";
+import Dialog from "@material-ui/core/Dialog";
+import IconButton from "@material-ui/core/IconButton";
+import Slide from "@material-ui/core/Slide";
+import { withStyles } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import CloseButton from "@material-ui/icons/Close";
+import * as React from "react";
+import FacultyProfilesController from "src/app/controllers/faculty_profiles";
+import IStyleClasses from "src/app/interfaces/style_classes";
+import styles from "./styles";
+
+const Transition = (props: any) => <Slide direction="up" {...props} />;
+
+interface IPropsType {
+    title: string;
+    open: boolean;
+    children: React.ReactNode;
+    classes: IStyleClasses;
+}
+
+class PrintPreviewDialog extends React.Component<IPropsType> {
+    public onClose = (shouldShow: boolean) => () => {
+        FacultyProfilesController.toggleProfilePrintPreview(shouldShow);
+    };
+
+    public render() {
+        const { title, open, children, classes } = this.props;
+        return (
+            <Dialog
+                fullScreen
+                open={open}
+                onClose={this.onClose(false)}
+                TransitionComponent={Transition}
+            >
+                <AppBar color="default" className={classes.appBar}>
+                    <Toolbar disableGutters>
+                        <IconButton
+                            onClick={this.onClose(false)}
+                            color="inherit"
+                        >
+                            <CloseButton />
+                        </IconButton>
+                        <Typography variant="headline" color="inherit">
+                            {title}
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <React.Fragment>{children}</React.Fragment>
+            </Dialog>
+        );
+    }
+}
+
+export default withStyles(styles)(PrintPreviewDialog);
