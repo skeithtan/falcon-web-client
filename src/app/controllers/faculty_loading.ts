438bceadf920af7e7212f87f4430f1f831b2b45c
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-16 14:59:06 +0800

    Add submit and set active term functions

--- src/app/controllers/faculty_loading.ts
+++ src/app/controllers/faculty_loading.ts
@@ -1,5 +1,6 @@
 import FacultyLoadingTab from "../models/enums/faculty_loading_tab";
 import FetchableStatus from "../models/enums/fetchable_status";
+import FormStatus from "../models/enums/form_status";
 import FacultyLoadingService from "../services/faculty_loading";
 import rootStore from "../store";
 import { groupById } from "../utils/group_by_id";
@@ -26,6 +27,35 @@ export default class FacultyLoadingController {
         if (!shouldShow) {
             facultyLoading.addTermState.resetAndClose();
         }
+    }
+
+    public static submitAddTerm() {
+        const { addTermState: formState } = facultyLoading;
+        const form = formState.form;
+        formState.setStatus(FormStatus.Submitting);
+
+        FacultyLoadingService.addTerm(form)
+            .then(t => {
+                facultyLoading.terms!.set(t.id, t);
+                formState.resetAndClose();
+                this.setActiveTerm(t.id);
+            })
+            .catch(e => {
+                formState.setStatus(FormStatus.Error, e);
+            });
+    }
+
+    public static setActiveTerm(id: number) {
+        facultyLoading.activeTermId = id;
+        const term = facultyLoading.activeTerm!;
+
+        FacultyLoadingService.fetchTerm(term.id)
+            .then(t => {
+                facultyLoading.terms!.set(t.id, t);
+            })
+            .catch((e: Error) =>
+                facultyLoading.setStatus(FetchableStatus.Error, e)
+            );
     }
 
     public static setActiveTab(tab: FacultyLoadingTab) {
