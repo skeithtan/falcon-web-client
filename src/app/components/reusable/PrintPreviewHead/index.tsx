a421e35bc6c6ac081ac8e8781b18146ac52eb423
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-16 10:30:40 +0800

    Create reusable print preview head
    The reason this is reusable is so we can use it too for printing the schedule things

---
+++ src/app/components/reusable/PrintPreviewHead/index.tsx
@@ -0,0 +1,42 @@
+import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import * as React from "react";
+import pnuLogo from "../../../../assets/images/pnu-logo.png";
+import IStyleClasses from "../../../interfaces/style_classes";
+import styles from "./styles";
+
+interface IPropsType {
+    classes: IStyleClasses;
+}
+
+class PrintPreviewHead extends React.Component<IPropsType> {
+    public render() {
+        const { classes } = this.props;
+        return (
+            <Grid container direction="row">
+                <Grid item>
+                    <img
+                        src={pnuLogo}
+                        alt="PNU Logo"
+                        className={classes.pnuLogo}
+                    />
+                </Grid>
+                <Grid item container direction="column">
+                    <Grid item>
+                        <Typography component="h5">
+                            Philippine Normal University
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <Typography component="h6" color="textSecondary">
+                            Faculty of Arts and Languages
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Grid>
+        );
+    }
+}
+
+export default withStyles(styles)(PrintPreviewHead);
