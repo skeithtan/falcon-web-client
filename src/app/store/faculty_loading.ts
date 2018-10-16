7f3ad8cffe8cbd39be7d911b8dabdcbf76603b9b
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-16 14:01:07 +0800

    Create faculty loading state

---
+++ src/app/store/faculty_loading.ts
@@ -0,0 +1,32 @@
+import { computed, observable } from "mobx";
+import FetchableState from "../interfaces/fetchable_state";
+import Term from "../models/entities/term";
+import FacultyLoadingTab from "../models/enums/faculty_loading_tab";
+import addTermFormState, {
+    AddTermFormState,
+} from "./faculty_loading/add_term_form";
+
+export class FacultyLoadingState extends FetchableState {
+    @observable
+    public terms?: Map<number, Term> = undefined;
+
+    @observable
+    public activeTermId?: number = undefined;
+
+    @observable
+    public activeTab: FacultyLoadingTab = FacultyLoadingTab.Overview;
+
+    @observable
+    public addTermState: AddTermFormState = addTermFormState;
+
+    @computed
+    get activeTerm() {
+        if (!this.activeTermId || !this.terms) {
+            return undefined;
+        }
+
+        return this.terms!.get(this.activeTermId);
+    }
+}
+
+export default new FacultyLoadingState();
