acfc747b6e3d816dc1b1398bba08867a283f648c
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-17 12:13:32 +0800

    Style loading detail

---
+++ src/app/pages/FacultyLoading/components/LoadingDetail/styles.ts
@@ -0,0 +1,12 @@
+import { StyleRules } from "@material-ui/core/styles";
+
+export default {
+    detail: {
+        height: "100%",
+        overflowY: "scroll",
+        overflowX: "hidden",
+    },
+    detailEmptyState: {
+        height: "100%",
+    },
+} as StyleRules;
