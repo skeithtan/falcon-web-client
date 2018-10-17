2c85a791064ff726900d7320c369b68ba9bdae5d
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-17 15:08:31 +0800

    Add subcomponents to loading bar

--- src/app/pages/FacultyLoading/components/LoadingDetail/components/LoadingBar/index.tsx
+++ src/app/pages/FacultyLoading/components/LoadingDetail/components/LoadingBar/index.tsx
@@ -1,35 +1,53 @@
 import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import { inject, observer } from "mobx-react";
 import * as React from "react";
 import FacultyLoadingController from "../../../../../../controllers/faculty_loading";
-import AddTermButton from "./components/AddTermButton";
-import TermSelect from "./components/TermSelect";
+import IStyleClasses from "../../../../../../interfaces/style_classes";
+import { FacultyLoadingState } from "../../../../../../store/faculty_loading";
 import TermTabs from "./components/TermTabs";
+import styles from "./styles";
 
-export default class LoadingBar extends React.Component {
-    public toggleAddTermForm = (shouldShow: boolean) => () => {
-        FacultyLoadingController.toggleAddTermForm(shouldShow);
+interface IPropsType {
+    facultyLoading?: FacultyLoadingState;
+    classes: IStyleClasses;
+}
+
+@inject("facultyLoading")
+@observer
+class LoadingBar extends React.Component<IPropsType> {
+    public toggleTermList = (shouldShow: boolean) => () => {
+        FacultyLoadingController.toggleTermList(shouldShow);
     };
 
     public render() {
+        const { facultyLoading, classes } = this.props;
+        const { activeTerm } = facultyLoading!;
         return (
-            <AppBar>
-                <Toolbar>
+            <AppBar color="default" position="static">
+                <Toolbar className={classes.toolbar}>
                     <Grid
                         container
                         direction="row"
                         wrap="nowrap"
-                        alignItems="stretch"
+                        alignItems="center"
+                        justify="space-between"
                     >
                         <Grid item>
-                            <TermSelect />
+                            <Button
+                                size="small"
+                                color="primary"
+                                onClick={this.toggleTermList(true)}
+                            >
+                                {activeTerm!.readableString}{" "}
+                                <ArrowDropDownIcon />
+                            </Button>
                         </Grid>
-                        <Grid item>
-                            <AddTermButton
-                                onAddClick={this.toggleAddTermForm(true)}
-                            />
-                        </Grid>
+
                         <Grid item>
                             <TermTabs />
                         </Grid>
@@ -39,3 +57,5 @@ export default class LoadingBar extends React.Component {
         );
     }
 }
+
+export default withStyles(styles)(LoadingBar);
