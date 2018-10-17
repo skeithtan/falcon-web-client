b8511c809f9376e45edb3fdc40e972331b0a5390
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-17 15:09:34 +0800

    Reformat add term form view

--- src/app/pages/FacultyLoading/components/AddTermFormView/index.tsx
+++ src/app/pages/FacultyLoading/components/AddTermFormView/index.tsx
@@ -1,21 +1,28 @@
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
 import Grid from "@material-ui/core/Grid";
 import MenuItem from "@material-ui/core/MenuItem";
+import { withStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import { inject, observer } from "mobx-react";
 import * as React from "react";
-import DrawerForm from "../../../../components/reusable/DrawerForm";
 import FormSubmitBar from "../../../../components/reusable/FormSubmitBar";
 import FacultyLoadingController from "../../../../controllers/faculty_loading";
+import IStyleClasses from "../../../../interfaces/style_classes";
 import { OrdinalTermReadable } from "../../../../models/enums/ordinal_term";
 import { FacultyLoadingState } from "../../../../store/faculty_loading";
+import styles from "./styles";
 
 interface IPropsType {
     facultyLoading?: FacultyLoadingState;
+    classes: IStyleClasses;
 }
 
 @inject("facultyLoading")
 @observer
-export default class AddTermFormView extends React.Component<IPropsType> {
+class AddTermFormView extends React.Component<IPropsType> {
     public onClose = () => {
         FacultyLoadingController.toggleAddTermForm(false);
     };
@@ -33,7 +40,7 @@ export default class AddTermFormView extends React.Component<IPropsType> {
     };
 
     public render() {
-        const { facultyLoading } = this.props;
+        const { facultyLoading, classes } = this.props;
         const {
             isShowing,
             form,
@@ -41,65 +48,63 @@ export default class AddTermFormView extends React.Component<IPropsType> {
             canSubmit,
         } = facultyLoading!.addTermState;
         return (
-            <DrawerForm
-                open={isShowing}
-                onClose={this.onClose}
-                formTitle="Add Term"
-            >
-                <Grid
-                    container
-                    spacing={24}
-                    alignItems="stretch"
-                    direction="column"
-                >
-                    <Grid item container spacing={8} direction="row">
-                        <Grid item xs>
-                            <TextField
-                                label="Start Year"
-                                variant="outlined"
-                                required
-                                onChange={this.onChange("startYear")}
-                                value={form.startYear}
-                                error={"startYear" in validationErrors}
-                                helperText={validationErrors.startYear}
-                                fullWidth
+            <Dialog open={isShowing} onClose={this.onClose} fullWidth>
+                <DialogTitle>Add Term</DialogTitle>
+                <DialogContent className={classes.content}>
+                    <Grid container direction="column">
+                        <Grid item container spacing={8} direction="row">
+                            <Grid item xs>
+                                <TextField
+                                    label="Start Year"
+                                    variant="outlined"
+                                    required
+                                    onChange={this.onChange("startYear")}
+                                    value={form.startYear}
+                                    error={"startYear" in validationErrors}
+                                    helperText={validationErrors.startYear}
+                                    fullWidth
+                                />
+                            </Grid>
+                            <Grid item xs={4}>
+                                <TextField
+                                    select
+                                    label="Term"
+                                    variant="outlined"
+                                    onChange={this.onChange("term")}
+                                    value={form.term}
+                                    error={"term" in validationErrors}
+                                    helperText={validationErrors.term}
+                                    fullWidth
+                                >
+                                    {Array.from(OrdinalTermReadable).map(
+                                        ([typeEnum, typeReadable]: any) => (
+                                            <MenuItem
+                                                key={typeEnum}
+                                                value={typeEnum}
+                                            >
+                                                {typeReadable}
+                                            </MenuItem>
+                                        )
+                                    )}
+                                </TextField>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </DialogContent>
+                <DialogActions className={classes.actions}>
+                    <Grid container direction="row-reverse">
+                        <Grid item>
+                            <FormSubmitBar
+                                disabled={!canSubmit}
+                                formState={facultyLoading!.addTermState}
+                                onSubmitClick={this.onSubmitClick}
                             />
                         </Grid>
                     </Grid>
-                    <Grid item container spacing={8} direction="row">
-                        <Grid item xs={4}>
-                            <TextField
-                                select
-                                label="Term"
-                                variant="outlined"
-                                onChange={this.onChange("term")}
-                                value={form.term}
-                                error={"term" in validationErrors}
-                                helperText={validationErrors.term}
-                                fullWidth
-                            >
-                                {Array.from(OrdinalTermReadable).map(
-                                    ([typeEnum, typeReadable]: any) => (
-                                        <MenuItem
-                                            key={typeEnum}
-                                            value={typeEnum}
-                                        >
-                                            {typeReadable}
-                                        </MenuItem>
-                                    )
-                                )}
-                            </TextField>
-                        </Grid>
-                    </Grid>
-                    <Grid item>
-                        <FormSubmitBar
-                            disabled={!canSubmit}
-                            formState={facultyLoading!.addTermState}
-                            onSubmitClick={this.onSubmitClick}
-                        />
-                    </Grid>
-                </Grid>
-            </DrawerForm>
+                </DialogActions>
+            </Dialog>
         );
     }
 }
+
+export default withStyles(styles)(AddTermFormView);
