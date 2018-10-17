fe48921b3cfd8dc4fc87c50d1e6f7780d722370c
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-17 15:07:41 +0800

    Add term list to loading detail

--- src/app/pages/FacultyLoading/components/LoadingDetail/index.tsx
+++ src/app/pages/FacultyLoading/components/LoadingDetail/index.tsx
@@ -1,10 +1,10 @@
 import Grid from "@material-ui/core/Grid";
 import { withStyles } from "@material-ui/core/styles";
-import { inject, observer } from "mobx-react";
 import * as React from "react";
 import IStyleClasses from "../../../../interfaces/style_classes";
 import { FacultyLoadingState } from "../../../../store/faculty_loading";
 import AddTermFormView from "../AddTermFormView";
+import TermList from "../TermList";
 import LoadingBar from "./components/LoadingBar";
 import styles from "./styles";
 
@@ -13,10 +13,9 @@ interface IPropsType {
     classes: IStyleClasses;
 }
 
-@inject("facultyLoading")
-@observer
 class LoadingDetail extends React.Component<IPropsType> {
     public render() {
+        const { classes } = this.props;
         return (
             <React.Fragment>
                 <Grid
@@ -24,11 +23,13 @@ class LoadingDetail extends React.Component<IPropsType> {
                     direction="column"
                     wrap="nowrap"
                     alignItems="stretch"
+                    className={classes.detail}
                 >
                     <Grid item>
                         <LoadingBar />
                     </Grid>
                 </Grid>
+                <TermList />
                 <AddTermFormView />
             </React.Fragment>
         );
