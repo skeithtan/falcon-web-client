c16c8932066d6b76e0a56da40c9c64e8699f4ffa
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-18 17:02:28 +0800

    Style loading detail

--- src/app/pages/FacultyLoading/components/LoadingDetail/styles.ts
+++ src/app/pages/FacultyLoading/components/LoadingDetail/styles.ts
@@ -3,14 +3,13 @@ import { StyleRules, Theme } from "@material-ui/core/styles";
 export default (theme: Theme): StyleRules => ({
     detail: {
         height: "100%",
-        overflowY: "scroll",
-        overflowX: "hidden",
+        overflow: "hidden",
     },
-    tab: {
+    tabGridItem: {
         height: "100%",
-        width: "90%",
-        margin: theme.spacing.unit * 4,
-        overflowY: "scroll",
-        overflowX: "hidden",
+        overflow: "hidden",
+    },
+    detailEmptyState: {
+        height: "100%",
     },
 });
