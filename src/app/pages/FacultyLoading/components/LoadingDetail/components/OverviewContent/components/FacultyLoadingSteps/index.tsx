8de1452f878950bb4a3d64304b1dedb59c81079d
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-18 11:18:34 +0800

    Create faculty loading stepper

---
+++ src/app/pages/FacultyLoading/components/LoadingDetail/components/OverviewContent/components/FacultyLoadingSteps/index.tsx
@@ -0,0 +1,30 @@
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
+import Stepper from "@material-ui/core/Stepper";
+import { inject, observer } from "mobx-react";
+import * as React from "react";
+import { TermStatusReadable } from "../../../../../../../../models/enums/term_status";
+import { FacultyLoadingState } from "../../../../../../../../store/faculty_loading";
+
+interface IPropsType {
+    facultyLoading?: FacultyLoadingState;
+}
+
+@inject("facultyLoading")
+@observer
+export default class FacultyLoadingSteps extends React.Component<IPropsType> {
+    public render() {
+        const { facultyLoading } = this.props;
+        return (
+            <Stepper activeStep={facultyLoading!.activeTabIndex}>
+                {Array.from(TermStatusReadable).map(
+                    ([statusEnum, readable]: any) => (
+                        <Step key={statusEnum}>
+                            <StepLabel>{readable}</StepLabel>
+                        </Step>
+                    )
+                )}
+            </Stepper>
+        );
+    }
+}
