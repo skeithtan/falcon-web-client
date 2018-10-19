ab77482ee6cc15594a91aabcf41d950b0d5e137b
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-19 15:25:19 +0800

    Change readable string

--- src/app/models/enums/meeting_hours.ts
+++ src/app/models/enums/meeting_hours.ts
@@ -8,12 +8,12 @@ enum MeetingHours {
 }
 
 export const MeetingHoursReadable = new Map<MeetingHours, string>([
-    [MeetingHours.AM_7_9, "AM_7_9"],
-    [MeetingHours.AM_9_11, "AM_9_11"],
-    [MeetingHours.AM_11_1, "AM_11_1"],
-    [MeetingHours.PM_1_3, "PM_1_3"],
-    [MeetingHours.PM_3_5, "PM_3_5"],
-    [MeetingHours.PM_5_7, "PM_5_7"],
+    [MeetingHours.AM_7_9, "7 - 9"],
+    [MeetingHours.AM_9_11, "9 - 11"],
+    [MeetingHours.AM_11_1, "11 - 1"],
+    [MeetingHours.PM_1_3, "1 - 3"],
+    [MeetingHours.PM_3_5, "3 - 5"],
+    [MeetingHours.PM_5_7, "5 - 7"],
 ]);
 
 export default MeetingHours;
