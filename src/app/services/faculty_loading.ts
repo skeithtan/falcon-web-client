fb506971d2dc180569b8a9da6505931f05ad0545
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-22 12:17:10 +0800

    Add fetch classes and add class schedule functions

--- src/app/services/faculty_loading.ts
+++ src/app/services/faculty_loading.ts
@@ -1,6 +1,8 @@
 import axios, { AxiosResponse } from "axios";
+import ClassSchedule from "../models/entities/class_schedule";
 import FacultyLoadingFacultyMember from "../models/entities/faculty_loading_faculty_member";
 import Term from "../models/entities/term";
+import AddClassForm from "../models/forms/add_class_form";
 import AddTermForm from "../models/forms/add_term_form";
 import { handleAxiosError } from "../utils/handle_axios_error";
 
@@ -38,6 +40,27 @@ export default class FacultyLoadingService {
                     (flfm: any) => new FacultyLoadingFacultyMember(flfm)
                 );
             })
+            .catch(handleAxiosError);
+    }
+
+    public static async fetchAllClasses(
+        termId: number
+    ): Promise<ClassSchedule[]> {
+        return axios
+            .get(`/terms/${termId}/class-schedules`)
+            .then((response: AxiosResponse) => {
+                return response.data.map((cs: any) => new ClassSchedule(cs));
+            })
+            .catch(handleAxiosError);
+    }
+
+    public static async addClassSchedule(
+        termId: number,
+        form: AddClassForm
+    ): Promise<ClassSchedule> {
+        return axios
+            .post(`/terms/${termId}/class-schedules`, form)
+            .then((response: AxiosResponse) => new ClassSchedule(response.data))
             .catch(handleAxiosError);
     }
 }
