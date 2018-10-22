9c1de8975825048112ef68b88a5d03f1a232bf81
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-22 14:35:11 +0800

    Create initial schedule column view

--- src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/components/ScheduleColumn/index.tsx
+++ src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/components/ScheduleColumn/index.tsx
@@ -1,13 +1,32 @@
+import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 import * as React from "react";
-import MeetingHours from "../../../../../../../../models/enums/meeting_hours";
+import IStyleClasses from "../../../../../../../../interfaces/style_classes";
+import ClassSchedule from "../../../../../../../../models/entities/class_schedule";
+import styles from "./styles";
 
 interface IPropsType {
-    meetingHour: MeetingHours;
+    classSchedules: ClassSchedule[];
+    classes: IStyleClasses;
 }
 
-export default class ScheduleColumn extends React.Component<IPropsType> {
+class ScheduleColumn extends React.Component<IPropsType> {
     public render() {
-        const { meetingHour } = this.props;
-        return <div>{meetingHour}</div>;
+        const { classSchedules, classes } = this.props;
+        return (
+            <Grid container direction="column">
+                {classSchedules.map(cs => (
+                    <div key={cs.id} className={classes.classSchedule}>
+                        <Typography variant="subtitle2">{`${cs.subjectCode} ${
+                            cs.section
+                        }`}</Typography>
+                        <Typography>{cs.room}</Typography>
+                    </div>
+                ))}
+            </Grid>
+        );
     }
 }
+
+export default withStyles(styles)(ScheduleColumn);
