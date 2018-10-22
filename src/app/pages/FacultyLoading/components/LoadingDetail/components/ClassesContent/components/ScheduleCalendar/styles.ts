2604a377b7bcf23b6b7df1af471ebdb4a68d25e2
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-22 14:35:19 +0800

    Style schedule calendar

--- src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/components/ScheduleCalendar/styles.ts
+++ src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/components/ScheduleCalendar/styles.ts
@@ -3,8 +3,21 @@ import { StyleRules, Theme } from "@material-ui/core/styles";
 export default (theme: Theme): StyleRules => ({
     root: {
         height: "100%",
+        backgroundColor: theme.palette.background.paper,
+    },
+    padded: {
+        paddingLeft: theme.spacing.unit * 3,
+        paddingRight: theme.spacing.unit * 3,
+    },
+    divider: {
+        width: "100%",
+    },
+    scheduleColumsGridItemContainer: {
+        paddingTop: theme.spacing.unit,
+        height: "100%",
     },
     meetingHourHeader: {
-        padding: theme.spacing.unit * 2,
+        paddingTop: theme.spacing.unit * 2,
+        paddingBottom: theme.spacing.unit,
     },
 });
