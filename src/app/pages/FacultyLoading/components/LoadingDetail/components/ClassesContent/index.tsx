46e3f274bc640b42858fa865da93e9692d39767e
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-22 14:34:41 +0800

    Style classes content

--- src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/index.tsx
+++ src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/index.tsx
@@ -1,31 +1,35 @@
 import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core/styles";
 import { inject, observer } from "mobx-react";
 import * as React from "react";
 import StateWrapper from "../../../../../../components/reusable/StateWrapper";
 import FacultyLoadingController from "../../../../../../controllers/faculty_loading";
+import IStyleClasses from "../../../../../../interfaces/style_classes";
 import { FacultyLoadingState } from "../../../../../../store/faculty_loading";
 import AddClassFormView from "./components/AddClassFormView";
 import ClassesAppBar from "./components/ClassesAppBar";
 import ScheduleCalendar from "./components/ScheduleCalendar";
+import styles from "./styles";
 
 interface IPropsType {
     facultyLoading?: FacultyLoadingState;
+    classes: IStyleClasses;
 }
 
 @inject("facultyLoading")
 @observer
-export default class ClassesContent extends React.Component<IPropsType> {
+class ClassesContent extends React.Component<IPropsType> {
     public componentDidMount() {
         FacultyLoadingController.getAllClassSchedulesTabPrerequisites();
     }
 
     public render() {
-        const { facultyLoading } = this.props;
+        const { facultyLoading, classes } = this.props;
         const { classesTabState } = facultyLoading!;
         return (
             <StateWrapper fetchableState={classesTabState.fetchStatus}>
                 {() => (
-                    <Grid container direction="column">
+                    <Grid container direction="column" className={classes.root}>
                         <Grid item>
                             <ClassesAppBar />
                         </Grid>
@@ -39,3 +43,5 @@ export default class ClassesContent extends React.Component<IPropsType> {
         );
     }
 }
+
+export default withStyles(styles)(ClassesContent);
