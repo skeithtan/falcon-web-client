5f33c04fcce671db6c682adcb57386db3bbe0ce8
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-22 14:35:34 +0800

    Modify components

--- src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/components/ScheduleCalendar/index.tsx
+++ src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/components/ScheduleCalendar/index.tsx
@@ -1,32 +1,71 @@
+import Divider from "@material-ui/core/Divider";
 import Grid from "@material-ui/core/Grid";
 import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import * as classNames from "classnames";
+import { inject, observer } from "mobx-react";
 import * as React from "react";
 import IStyleClasses from "../../../../../../../../interfaces/style_classes";
-import MeetingHours from "../../../../../../../../models/enums/meeting_hours";
+import MeetingHours, { MeetingHoursReadable } from "../../../../../../../../models/enums/meeting_hours";
+import { FacultyLoadingState } from "../../../../../../../../store/faculty_loading";
 import ScheduleColumn from "../ScheduleColumn";
 import styles from "./styles";
 
 interface IPropsType {
     classes: IStyleClasses;
+    facultyLoading?: FacultyLoadingState;
 }
 
+@inject("facultyLoading")
+@observer
 class ScheduleCalendar extends React.Component<IPropsType> {
     public render() {
-        const { classes } = this.props;
+        const { classes, facultyLoading } = this.props;
+        const classSchedules = facultyLoading!.classesTabState
+            .activeMeetingDaysClassSchedules;
+
         return (
-            <Grid container className={classes.root}>
-                <Grid item container direction="row">
-                    {Array.from(Object.keys(MeetingHours)).map(mhr => (
-                        <Grid item xs key={mhr}>
-                            Hello, World. {mhr}
-                        </Grid>
-                    ))}
+            <Grid container alignItems="stretch" className={classes.root}>
+                <Grid item className={classes.padded} container direction="row">
+                    {Array.from(Object.keys(MeetingHours)).map(
+                        (mhr: MeetingHours) => (
+                            <Grid
+                                item
+                                container
+                                xs
+                                justify="center"
+                                key={mhr}
+                                className={classes.meetingHourHeader}
+                            >
+                                <Typography
+                                    variant="overline"
+                                    color="textSecondary"
+                                >
+                                    {MeetingHoursReadable.get(mhr)}
+                                </Typography>
+                            </Grid>
+                        )
+                    )}
                 </Grid>
-                <Grid item container>
+                <Grid item className={classes.divider}>
+                    <Divider />
+                </Grid>
+                <Grid
+                    item
+                    className={classNames(
+                        classes.scheduleColumsGridItemContainer,
+                        classes.padded
+                    )}
+                    container
+                >
                     {Array.from(Object.keys(MeetingHours)).map(
                         (mhr: MeetingHours) => (
                             <Grid item xs key={mhr}>
-                                <ScheduleColumn meetingHour={mhr} />
+                                <ScheduleColumn
+                                    classSchedules={classSchedules.filter(
+                                        cs => cs.meetingHours === mhr
+                                    )}
+                                />
                             </Grid>
                         )
                     )}
