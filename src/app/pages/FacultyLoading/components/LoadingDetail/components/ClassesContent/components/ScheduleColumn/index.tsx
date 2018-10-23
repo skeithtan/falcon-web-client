be857303c4052de5d93f310380a3591ba756fb5d
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-23 10:08:40 +0800

    Add show class schedule details function

--- src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/components/ScheduleColumn/index.tsx
+++ src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/components/ScheduleColumn/index.tsx
@@ -2,6 +2,7 @@ import Grid from "@material-ui/core/Grid";
 import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import * as React from "react";
+import FacultyLoadingController from "../../../../../../../../controllers/faculty_loading";
 import IStyleClasses from "../../../../../../../../interfaces/style_classes";
 import ClassSchedule from "../../../../../../../../models/entities/class_schedule";
 import styles from "./styles";
@@ -12,6 +13,14 @@ interface IPropsType {
 }
 
 class ScheduleColumn extends React.Component<IPropsType> {
+    public showClassScheduleDetails = (
+        cs: number,
+        shouldShow: boolean
+    ) => () => {
+        FacultyLoadingController.setSelectedClassSchedule(cs);
+        FacultyLoadingController.toggleClassScheduleDetails(shouldShow);
+    };
+
     public render() {
         const { classSchedules, classes } = this.props;
         return (
@@ -23,7 +32,11 @@ class ScheduleColumn extends React.Component<IPropsType> {
                 spacing={16}
             >
                 {classSchedules.map(cs => (
-                    <Grid item key={cs.id}>
+                    <Grid
+                        item
+                        key={cs.id}
+                        onClick={this.showClassScheduleDetails(cs.id, true)}
+                    >
                         <div className={classes.classSchedule}>
                             <Typography variant="subtitle2">{`${
                                 cs.subjectCode
