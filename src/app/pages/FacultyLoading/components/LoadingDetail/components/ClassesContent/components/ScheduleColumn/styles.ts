7a38985736cb201dc57617c49fb9ba0b34161b86
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-24 11:18:47 +0800

    Remove class schedule style
    It went from a div to a Card

--- src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/components/ScheduleColumn/styles.ts
+++ src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/components/ScheduleColumn/styles.ts
@@ -1,15 +1,8 @@
-import grey from "@material-ui/core/colors/grey";
 import { StyleRules, Theme } from "@material-ui/core/styles";
 
 export default (theme: Theme): StyleRules => ({
     padded: {
         paddingLeft: theme.spacing.unit,
         paddingRight: theme.spacing.unit,
-    },
-    classSchedule: {
-        padding: theme.spacing.unit * 2,
-        border: "1px solid",
-        borderColor: grey["300"],
-        cursor: "pointer",
     },
 });
