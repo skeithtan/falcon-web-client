0127f502dc1879de67fa2f9675d0919703bfb649
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-29 21:08:33 +0800

    Style overview content

--- src/app/pages/FacultyLoading/components/LoadingDetail/components/OverviewContent/styles.ts
+++ src/app/pages/FacultyLoading/components/LoadingDetail/components/OverviewContent/styles.ts
@@ -2,6 +2,14 @@ import { StyleRules, Theme } from "@material-ui/core/styles";
 
 export default (theme: Theme): StyleRules => ({
     root: {
+        height: "100%",
         backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing.unit * 5,
+        display: "flex",
+        justifyContent: "center",
+    },
+    gridContainer: {
+        height: "100%",
+        maxWidth: 1600,
     },
 });
