206e0073c65258b95e3ebec7556f425f1be3a935
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-29 14:38:18 +0800

    Rename faculty member

--- src/app/services/faculty_members.ts
+++ src/app/services/faculty_members.ts
@@ -1,53 +1,53 @@
 import axios, { AxiosResponse } from "axios";
-import FacultyMember from "../models/entities/faculty_member";
+import FacultyProfile from "../models/entities/faculty_profile";
 import AddFacultyMemberForm from "../models/forms/add_faculty_member_form";
 import UpdateFacultyMemberForm from "../models/forms/update_faculty_member_form";
 import { handleAxiosError } from "../utils/handle_axios_error";
 
 export default class FacultyMembersService {
-    public static async fetchAllFacultyMembers(): Promise<FacultyMember[]> {
+    public static async fetchAllFacultyMembers(): Promise<FacultyProfile[]> {
         return axios
             .get("/faculty-members")
             .then((response: AxiosResponse) => {
-                return response.data.map((fm: any) => new FacultyMember(fm));
+                return response.data.map((fm: any) => new FacultyProfile(fm));
             })
             .catch(handleAxiosError);
     }
 
     public static async fetchFacultyMember(
         facultyId: number
-    ): Promise<FacultyMember> {
+    ): Promise<FacultyProfile> {
         return axios
             .get(`/faculty-members/${facultyId}`)
-            .then((response: AxiosResponse) => new FacultyMember(response.data))
+            .then((response: AxiosResponse) => new FacultyProfile(response.data))
             .catch(handleAxiosError);
     }
 
     public static async addFacultyMember(
         form: AddFacultyMemberForm
-    ): Promise<FacultyMember> {
+    ): Promise<FacultyProfile> {
         return axios
             .post("/faculty-members/", form)
-            .then((response: AxiosResponse) => new FacultyMember(response.data))
+            .then((response: AxiosResponse) => new FacultyProfile(response.data))
             .catch(handleAxiosError);
     }
 
     public static async updateFacultyMember(
         form: UpdateFacultyMemberForm,
         activeId: number
-    ): Promise<FacultyMember> {
+    ): Promise<FacultyProfile> {
         return axios
             .put(`/faculty-members/${activeId}`, form)
-            .then((response: AxiosResponse) => new FacultyMember(response.data))
+            .then((response: AxiosResponse) => new FacultyProfile(response.data))
             .catch(handleAxiosError);
     }
 
-    public static async fetchCurrentFaculty(): Promise<FacultyMember> {
+    public static async fetchCurrentFaculty(): Promise<FacultyProfile> {
         return axios
             .get("/faculty-members/current")
             .then(
                 (response: AxiosResponse<any>) =>
-                    new FacultyMember(response.data)
+                    new FacultyProfile(response.data)
             )
             .catch(handleAxiosError);
     }
