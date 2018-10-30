56ef043f45c62e3e6c3434617548d849d876fbbe
hideyarguelles <catherine_arguelles@yahoo.com>
2018-10-30 15:07:16 +0800

    Create initial list for faculty candidates

--- src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/components/ClassScheduleDetailsDrawer/index.tsx
+++ src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/components/ClassScheduleDetailsDrawer/index.tsx
@@ -1,16 +1,22 @@
 import Button from "@material-ui/core/Button";
 import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
 import Divider from "@material-ui/core/Divider";
 import Grid from "@material-ui/core/Grid";
 import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
 import { inject, observer } from "mobx-react";
 import * as React from "react";
 import DetailItem from "../../../../../../../../components/reusable/DetailItem";
 import DrawerForm from "../../../../../../../../components/reusable/DrawerForm";
 import FacultyLoadingController from "../../../../../../../../controllers/faculty_loading";
 import ClassSchedule from "../../../../../../../../models/entities/class_schedule";
-import MeetingDays, { MeetingDaysReadable } from "../../../../../../../../models/enums/meeting_days";
-import MeetingHours, { MeetingHoursReadable } from "../../../../../../../../models/enums/meeting_hours";
+import MeetingDays, {
+    MeetingDaysReadable,
+} from "../../../../../../../../models/enums/meeting_days";
+import MeetingHours, {
+    MeetingHoursReadable,
+} from "../../../../../../../../models/enums/meeting_hours";
 import { FacultyLoadingState } from "../../../../../../../../store/faculty_loading";
 
 interface IPropsType {
@@ -35,11 +41,18 @@ export default class ClassScheduleDetailsDrawer extends React.Component<
         }
     };
 
+    public onAssignFacultyClick = () => {
+        // TODO: this
+    };
+
     public renderActiveClassSchedule = () => {
         const { facultyLoading } = this.props;
         const { classesTabState } = facultyLoading!;
-        const { activeClassSchedule } = classesTabState;
-
+        const {
+            activeClassSchedule,
+            classScheduleDetailsState,
+        } = classesTabState;
+        const { form } = classScheduleDetailsState;
         return (
             <Grid container direction="column" spacing={16} wrap="nowrap">
                 <Grid item>
@@ -95,7 +108,31 @@ export default class ClassScheduleDetailsDrawer extends React.Component<
                     </CardActions>
                 </Grid>
 
-                <Divider />
+                <Grid item>
+                    <Divider />
+                </Grid>
+
+                <Grid item container direction="column" spacing={16}>
+                    <CardContent>
+                        <Typography variant="h6">Faculty Members</Typography>
+                        <Typography variant="caption">
+                            The faculty members are arranged according to their
+                            eligibility for assignment to the class.
+                        </Typography>
+
+                        {/* TODO: Sorted list of faculty candidate items */}
+                        
+                    </CardContent>
+                    <CardActions>
+                        <Button
+                            disabled={form.facultyMember === undefined}
+                            onClick={this.onAssignFacultyClick}
+                            color="primary"
+                        >
+                            Assign Faculty Member
+                        </Button>
+                    </CardActions>
+                </Grid>
             </Grid>
         );
     };
