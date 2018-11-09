b6c08ed6594bf75124630745b2177ec8fb026631
hideyarguelles <catherine_arguelles@yahoo.com>
2018-11-09 16:38:52 +0800

    Add state wrapper

--- src/app/pages/FacultyLoading/components/LoadingDetail/index.tsx
+++ src/app/pages/FacultyLoading/components/LoadingDetail/index.tsx
@@ -2,6 +2,7 @@ import Grid from "@material-ui/core/Grid";
 import { withStyles } from "@material-ui/core/styles";
 import { inject, observer } from "mobx-react";
 import * as React from "react";
+import StateWrapper from "../../../../components/reusable/StateWrapper";
 import IStyleClasses from "../../../../interfaces/style_classes";
 import FacultyLoadingTab from "../../../../models/enums/faculty_loading_tab";
 import { FacultyLoadingState } from "../../../../store/faculty_loading";
@@ -25,30 +26,34 @@ class LoadingDetail extends React.Component<IPropsType> {
         const { facultyLoading, classes } = this.props;
         const { activeTab } = facultyLoading!;
         return (
-            <Grid
-                container
-                direction="column"
-                wrap="nowrap"
-                alignItems="stretch"
-                className={classes.detail}
-            >
-                <Grid item xs>
-                    <LoadingBar />
-                </Grid>
-                <Grid item xs="auto" className={classes.tabGridItem}>
-                    {activeTab === FacultyLoadingTab.Overview && (
-                        <OverviewContent />
-                    )}
-                    {activeTab === FacultyLoadingTab.Faculty && (
-                        <FacultyContent />
-                    )}
-                    {activeTab === FacultyLoadingTab.Classes && (
-                        <ClassesContent />
-                    )}
-                </Grid>
-                <TermList />
-                <AddTermFormView />
-            </Grid>
+            <StateWrapper fetchableState={facultyLoading!.fetchStatus}>
+                {() => (
+                    <Grid
+                        container
+                        direction="column"
+                        wrap="nowrap"
+                        alignItems="stretch"
+                        className={classes.detail}
+                    >
+                        <Grid item xs>
+                            <LoadingBar />
+                        </Grid>
+                        <Grid item xs="auto" className={classes.tabGridItem}>
+                            {activeTab === FacultyLoadingTab.Overview && (
+                                <OverviewContent />
+                            )}
+                            {activeTab === FacultyLoadingTab.Faculty && (
+                                <FacultyContent />
+                            )}
+                            {activeTab === FacultyLoadingTab.Classes && (
+                                <ClassesContent />
+                            )}
+                        </Grid>
+                        <TermList />
+                        <AddTermFormView />
+                    </Grid>
+                )}
+            </StateWrapper>
         );
     }
 }
