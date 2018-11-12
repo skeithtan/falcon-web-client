e70adc84228048a8e11b3115f5e8102f8d643a8c
hideyarguelles <catherine_arguelles@yahoo.com>
2018-11-12 13:59:51 +0800

    Add authentication and phase restriction

--- src/app/pages/FacultyLoading/components/LoadingDetail/components/OverviewContent/index.tsx
+++ src/app/pages/FacultyLoading/components/LoadingDetail/components/OverviewContent/index.tsx
@@ -7,17 +7,20 @@ import * as React from "react";
 import FacultyLoadingController from "../../../../../../controllers/faculty_loading";
 import IStyleClasses from "../../../../../../interfaces/style_classes";
 import TermStatus from "../../../../../../models/enums/term_status";
+import UserType from "../../../../../../models/enums/user_type";
+import { AuthenticationState } from "../../../../../../store/authentication";
 import { FacultyLoadingState } from "../../../../../../store/faculty_loading";
 import FacultyLoadingSteps from "./components/FacultyLoadingSteps";
 import TrackedMetricsView from "./components/TrackedMetricsView";
 import styles from "./styles";
 
 interface IPropsType {
+    authentication?: AuthenticationState;
     facultyLoading?: FacultyLoadingState;
     classes: IStyleClasses;
 }
 
-@inject("facultyLoading")
+@inject("facultyLoading", "authentication")
 @observer
 class OverviewContent extends React.Component<IPropsType> {
     public onPreviousClick = () => {
@@ -33,8 +36,9 @@ class OverviewContent extends React.Component<IPropsType> {
     };
 
     public render() {
-        const { facultyLoading, classes } = this.props;
+        const { facultyLoading, authentication, classes } = this.props;
         const { activeTerm } = facultyLoading!;
+        const { currentUser } = authentication!;
         return (
             <div className={classes.root}>
                 <Grid
@@ -57,51 +61,53 @@ class OverviewContent extends React.Component<IPropsType> {
                                 {facultyLoading!.activeTerm!.readable}
                             </Typography>
                         </Grid>
-                        {activeTerm!.status !== TermStatus.Archived && (
-                            <Grid
-                                item
-                                container
-                                direction="row"
-                                justify="flex-end"
-                                alignItems="center"
-                                wrap="nowrap"
-                                spacing={8}
-                                xs={6}
-                            >
-                                <Grid item>
-                                    <Button
-                                        color="primary"
-                                        variant="outlined"
-                                        disabled={
-                                            activeTerm!.status ===
-                                            TermStatus.Initializing
-                                        }
-                                        onClick={this.onPreviousClick}
-                                    >
-                                        {activeTerm!.status ===
-                                        TermStatus.Published
-                                            ? "Unpublish"
-                                            : "Back"}
-                                    </Button>
-                                </Grid>
-                                <Grid item>
-                                    <Button
-                                        color="primary"
-                                        variant="contained"
-                                        disabled={
-                                            activeTerm!.status ===
+                        {activeTerm!.status !== TermStatus.Archived &&
+                            currentUser!.authorization ===
+                                UserType.AssociateDean && (
+                                <Grid
+                                    item
+                                    container
+                                    direction="row"
+                                    justify="flex-end"
+                                    alignItems="center"
+                                    wrap="nowrap"
+                                    spacing={8}
+                                    xs={6}
+                                >
+                                    <Grid item>
+                                        <Button
+                                            color="primary"
+                                            variant="outlined"
+                                            disabled={
+                                                activeTerm!.status ===
+                                                TermStatus.Initializing
+                                            }
+                                            onClick={this.onPreviousClick}
+                                        >
+                                            {activeTerm!.status ===
                                             TermStatus.Published
-                                        }
-                                        onClick={this.onProceedClick}
-                                    >
-                                        {activeTerm!.status ===
-                                        TermStatus.FeedbackGathering
-                                            ? "Publish"
-                                            : "Proceed"}
-                                    </Button>
+                                                ? "Unpublish"
+                                                : "Back"}
+                                        </Button>
+                                    </Grid>
+                                    <Grid item>
+                                        <Button
+                                            color="primary"
+                                            variant="contained"
+                                            disabled={
+                                                activeTerm!.status ===
+                                                TermStatus.Published
+                                            }
+                                            onClick={this.onProceedClick}
+                                        >
+                                            {activeTerm!.status ===
+                                            TermStatus.FeedbackGathering
+                                                ? "Publish"
+                                                : "Proceed"}
+                                        </Button>
+                                    </Grid>
                                 </Grid>
-                            </Grid>
-                        )}
+                            )}
                     </Grid>
                     <Grid item>
                         <FacultyLoadingSteps />
