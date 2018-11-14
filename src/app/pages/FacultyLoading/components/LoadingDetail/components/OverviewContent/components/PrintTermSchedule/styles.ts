00b308cca479fbf3b90fa9bde938faf6d58ba4fd
hideyarguelles <catherine_arguelles@yahoo.com>
2018-11-14 11:11:04 +0800

    Style print term schedule

---
+++ src/app/pages/FacultyLoading/components/LoadingDetail/components/OverviewContent/components/PrintTermSchedule/styles.ts
@@ -0,0 +1,19 @@
+import { StyleRules, Theme } from "@material-ui/core/styles";
+
+export default (theme: Theme): StyleRules => ({
+    root: {
+        height: "inherit",
+        padding: theme.spacing.unit * 10,
+    },
+    paper: {
+        minHeight: 1000,
+        minWidth: 1200,
+        backgroundColor: theme.palette.background.paper,
+        margin: "auto",
+    },
+    printButton: {
+        position: "absolute",
+        bottom: 40,
+        right: 40,
+    },
+});
