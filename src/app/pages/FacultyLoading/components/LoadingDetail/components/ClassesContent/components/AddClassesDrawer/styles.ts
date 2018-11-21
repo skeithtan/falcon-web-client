2275fd9c53225b483a784b6226c8551128648a0d
hideyarguelles <catherine_arguelles@yahoo.com>
2018-11-21 14:31:34 +0800

    Style add classes drawer

---
+++ src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/components/AddClassesDrawer/styles.ts
@@ -0,0 +1,12 @@
+import { StyleRules } from "@material-ui/core/styles";
+
+export default {
+    drawer: {
+        minWidth: 800,
+        width: "70vw",
+        maxWidth: 1200,
+    },
+    table: {
+        width: "100%",
+    },
+} as StyleRules;
