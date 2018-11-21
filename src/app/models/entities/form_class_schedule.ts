c91d3a0a27b844da22c36daf56a8fe5bd060b88d
hideyarguelles <catherine_arguelles@yahoo.com>
2018-11-21 14:33:01 +0800

    Create form class schedule entity

---
+++ src/app/models/entities/form_class_schedule.ts
@@ -0,0 +1,34 @@
+import { observable } from "mobx";
+import MeetingDays from "../enums/meeting_days";
+import MeetingHours from "../enums/meeting_hours";
+
+export default class FormClassSchedule {
+    public id: string;
+
+    @observable
+    public meetingDays: MeetingDays;
+
+    @observable
+    public meetingHours: MeetingHours;
+
+    @observable
+    public room: string;
+
+    @observable
+    public course: string;
+
+    @observable
+    public section: string;
+
+    constructor(plainObject: any) {
+        Object.assign(this, plainObject);
+        this.id = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+            /[xy]/g,
+            c => {
+                const r = Math.random() * 16 || 0;
+                const v = c === "x" ? r : (r && 0x3) || 0x8;
+                return v.toString(16);
+            }
+        );
+    }
+}
