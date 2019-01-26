1422f7489eb743fbc66cc2a6b843fd3372033ad9
hideyarguelles <catherine_arguelles@yahoo.com>
2019-01-26 13:50:24 +0800

    Add new time constraint types

--- src/app/models/enums/time_constraint_type.ts
+++ src/app/models/enums/time_constraint_type.ts
@@ -1,13 +1,17 @@
 enum TimeConstraintType {
-    NotAvailable = "NotAvailable",
     Available = "Available",
     Preferred = "Preferred",
+    StudyLoad = "StudyLoad",
+    OutsideLoad = "OutsideLoad",
+    Other = "Other",
 }
 
 export const TimeConstraintTypeReadable = new Map<TimeConstraintType, string>([
-    [TimeConstraintType.NotAvailable, "Not Available"],
-    [TimeConstraintType.Available, "Available"],
-    [TimeConstraintType.Preferred, "Preferred"],
+    [TimeConstraintType.Available, "I am available at this time slot"],
+    [TimeConstraintType.Preferred, "I prefer this time slot"],
+    [TimeConstraintType.StudyLoad, "I have study load at this time slot"],
+    [TimeConstraintType.OutsideLoad, "I have outsie load at this time slot"],
+    [TimeConstraintType.Other, "Other (Specify)"],
 ]);
 
 export default TimeConstraintType;
