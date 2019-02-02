5bcc4f23ce00c684adbbbc42c1a3f8875f2cc5fb
hideyarguelles <catherine_arguelles@yahoo.com>
2019-02-02 16:52:08 +0800

    Add initial term schedule print settings

---
+++ src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/components/PrintTermSchedule/components/PrintSettings/index.tsx
@@ -0,0 +1,12 @@
+import TextField from "@material-ui/core/TextField";
+import * as React from "react";
+
+export default class PrintSettings extends React.Component {
+    public render() {
+        return (
+            <div>
+                <TextField />
+            </div>
+        );
+    }
+}
