f33e53385c7824fdd0bb1e9b22933d983f50ce53
hideyarguelles <catherine_arguelles@yahoo.com>
2019-02-16 17:23:46 +0800

    Add custom validator for completion year

--- src/app/models/forms/add_degree_form.ts
+++ src/app/models/forms/add_degree_form.ts
@@ -1,7 +1,35 @@
-import { IsEnum, IsNotEmpty } from "class-validator";
+import {
+    IsEnum,
+    IsNotEmpty,
+    registerDecorator,
+    ValidationArguments,
+    ValidationOptions,
+    Validator,
+} from "class-validator";
 import { observable } from "mobx";
 import { AddSubdocumentForm } from "../../interfaces/add_subdocument_form";
 import DegreeLevel from "../enums/degree_level";
+
+const validator = new Validator();
+
+function validateCompletionYear(validationOptions?: ValidationOptions) {
+    return (object: object, propertyName: string) => {
+        registerDecorator({
+            name: "isLongerThan",
+            target: object.constructor,
+            propertyName,
+            options: validationOptions,
+            validator: {
+                validate(value: any, args: ValidationArguments) {
+                    return value === undefined
+                        ? true
+                        : validator.length(value, 4, 4) &&
+                              validator.isNumberString(value);
+                },
+            },
+        });
+    };
+}
 
 export default class AddDegreeForm extends AddSubdocumentForm {
     @IsNotEmpty({
@@ -17,6 +45,7 @@ export default class AddDegreeForm extends AddSubdocumentForm {
     @observable
     public level: string = "";
 
+    @validateCompletionYear()
     @observable
     public completionYear?: string = "";
 
