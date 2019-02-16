fc3d19b820439e1bb5616d81abb9d0070ef3da5c
hideyarguelles <catherine_arguelles@yahoo.com>
2019-02-16 10:36:49 +0800

    Add course filter

--- src/app/store/faculty_loading/print_term_schedule.ts
+++ src/app/store/faculty_loading/print_term_schedule.ts
@@ -3,6 +3,9 @@ import { observable } from "mobx";
 export class PrintTermScheduleState {
     @observable
     public isShowing: boolean = false;
+    
+    @observable
+    public courseFilter: string = "";
 }
 
 export default new PrintTermScheduleState();
