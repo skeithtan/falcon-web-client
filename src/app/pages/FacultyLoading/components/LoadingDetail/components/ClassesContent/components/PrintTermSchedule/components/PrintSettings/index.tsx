64cfcefc8550e5098a0b532f3e7c832f783baf93
hideyarguelles <catherine_arguelles@yahoo.com>
2019-02-19 20:03:27 +0800

    Add subject filtering in settings

--- src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/components/PrintTermSchedule/components/PrintSettings/index.tsx
+++ src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/components/PrintTermSchedule/components/PrintSettings/index.tsx
@@ -14,7 +14,7 @@ interface IPropsType {
 @inject("facultyLoading")
 @observer
 export default class PrintSettings extends React.Component<IPropsType> {
-    public onChange = (event: any) => {
+    public onCourseChange = (event: any) => {
         const { facultyLoading } = this.props;
         const { classesTabState } = facultyLoading!;
         const { printTermScheduleState } = classesTabState;
@@ -24,20 +24,34 @@ export default class PrintSettings extends React.Component<IPropsType> {
         printTermScheduleState.courseFilter = event.target.value;
     };
 
+    public onSubjectChange = (event: any) => {
+        const { facultyLoading } = this.props;
+        const { classesTabState } = facultyLoading!;
+        const { printTermScheduleState } = classesTabState;
+        if (event.target.value === "None") {
+            printTermScheduleState.subjectFilter = "";
+        }
+        printTermScheduleState.subjectFilter = event.target.value;
+    };
+
     public render() {
         const { facultyLoading } = this.props;
         const { classesTabState } = facultyLoading!;
         const {
             classSchedules,
-            printTermScheduleState: { courseFilter },
+            printTermScheduleState: { courseFilter, subjectFilter },
         } = classesTabState;
         const noCourseFilter = courseFilter === "";
+        const noSubjectFilter = subjectFilter === "";
 
         const courses: string[] = [];
+        const subjects: string[] = [];
         Array.from(classSchedules!.values()).map(cs => {
             courses.push(cs.course);
+            subjects.push(cs.subjectCode);
         });
         const uniqueCourses = _.uniq(courses);
+        const uniqueSubjects = _.uniq(subjects);
 
         let filteredCourses = [];
         filteredCourses = uniqueCourses;
@@ -47,8 +61,19 @@ export default class PrintSettings extends React.Component<IPropsType> {
             );
         }
 
+        let filteredSubjects = [];
+        filteredSubjects = uniqueSubjects;
+        if (!noSubjectFilter) {
+            filteredSubjects = uniqueSubjects.filter(
+                subject => subject === subjectFilter
+            );
+        }
+
         return (
             <Grid container direction="column" spacing={24}>
+                <Grid item>
+                    <Typography variant="h6">Filter by school year</Typography>
+                </Grid>
                 <Grid item>
                     <Typography variant="h6">Filter by course</Typography>
                     <TextField
@@ -56,7 +81,7 @@ export default class PrintSettings extends React.Component<IPropsType> {
                         variant="outlined"
                         value={courseFilter || ""}
                         select
-                        onChange={this.onChange}
+                        onChange={this.onCourseChange}
                         fullWidth
                     >
                         <MenuItem value="">None</MenuItem>
@@ -69,7 +94,23 @@ export default class PrintSettings extends React.Component<IPropsType> {
                     </TextField>
                 </Grid>
                 <Grid item>
-                    <Typography variant="h6">Filter by school year</Typography>
+                    <Typography variant="h6">Filter by subject</Typography>
+                    <TextField
+                        label="Subject"
+                        variant="outlined"
+                        value={subjectFilter || ""}
+                        select
+                        onChange={this.onSubjectChange}
+                        fullWidth
+                    >
+                        <MenuItem value="">None</MenuItem>
+                        {filteredSubjects &&
+                            filteredSubjects.map(subject => (
+                                <MenuItem key={subject} value={subject}>
+                                    {subject}
+                                </MenuItem>
+                            ))}
+                    </TextField>
                 </Grid>
             </Grid>
         );
