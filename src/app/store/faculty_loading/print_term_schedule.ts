ae25708e60bdfdea228f8f0a8c4929f3de597f49
hideyarguelles <catherine_arguelles@yahoo.com>
2019-02-23 11:48:36 +0800

    Add year filter

--- src/app/store/faculty_loading/print_term_schedule.ts
+++ src/app/store/faculty_loading/print_term_schedule.ts
@@ -3,6 +3,9 @@ import { observable } from "mobx";
 export class PrintTermScheduleState {
     @observable
     public isShowing: boolean = false;
+
+    @observable
+    public yearFilter: number = 0;
     
     @observable
     public courseFilter: string = "";
