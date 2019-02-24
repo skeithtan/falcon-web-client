91b4036227cf5d26f3c613bbe87f45b3ad22d666
hideyarguelles <catherine_arguelles@yahoo.com>
2019-02-24 09:13:04 +0800

    Move react to print outside

--- src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/components/PrintTermSchedule/components/PrintPreview/index.tsx
+++ src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/components/PrintTermSchedule/components/PrintPreview/index.tsx
@@ -1,4 +1,3 @@
-import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import { withStyles } from "@material-ui/core/styles";
@@ -14,9 +13,6 @@ import { FacultyLoadingState } from "../../../../../../../../../../store/faculty
 import ScheduleTable from "../ScheduleTable";
 import styles from "./styles";
 
-// tslint:disable-next-line
-const ReactToPrint = require("react-to-print");
-
 interface IPropsType {
     facultyLoading?: FacultyLoadingState;
     classes: IStyleClasses;
@@ -27,83 +23,56 @@ interface IPropsType {
 
 @observer
 class PrintPreview extends React.Component<IPropsType> {
-    public printRef?: any;
-
-    public getTrigger = () => {
-        const { classes } = this.props;
-        return (
-            <Button variant="extendedFab" className={classes.printButton}>
-                Print Schedule
-            </Button>
-        );
-    };
-
-    public getPrintContent = () => this.printRef;
-
     public render() {
         const { classes, term, startYear, classSchedules } = this.props;
         return (
             <Paper className={classes.paper}>
-                <div
-                    ref={(el: any) => (this.printRef = el)}
-                    className={classes.printContentContainer}
-                >
+                <Grid container direction="column" wrap="nowrap" spacing={24}>
+                    <Grid item>
+                        <PrintPreviewHead />
+                    </Grid>
                     <Grid
+                        item
                         container
                         direction="column"
-                        wrap="nowrap"
-                        spacing={24}
+                        justify="center"
+                        alignItems="center"
                     >
                         <Grid item>
-                            <PrintPreviewHead />
+                            <Typography variant="h6">
+                                {`Schedule for ${term} Term ${startYear} - ${startYear +
+                                    1}`}
+                            </Typography>
                         </Grid>
-                        <Grid
-                            item
-                            container
-                            direction="column"
-                            justify="center"
-                            alignItems="center"
-                        >
-                            <Grid item>
-                                <Typography variant="h6">
-                                    {`Schedule for ${term} Term ${startYear} - ${startYear +
-                                        1}`}
-                                </Typography>
-                            </Grid>
-                            <Grid item>
-                                <Typography
-                                    variant="overline"
-                                    color="textSecondary"
-                                >
-                                    Generated {moment().format("LLLL")}
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                        <Grid item container direction="column" spacing={24}>
-                            {classSchedules &&
-                                Array.from(MeetingDaysReadable).map(
-                                    ([mdrEnum, mdrStr]) => {
-                                        const dayClasses = classSchedules!.filter(
-                                            cs => cs.meetingDays === mdrEnum
-                                        );
-
-                                        return (
-                                            <Grid item key={mdrEnum}>
-                                                <ScheduleTable
-                                                    meetingDays={mdrStr}
-                                                    classSchedules={dayClasses}
-                                                />
-                                            </Grid>
-                                        );
-                                    }
-                                )}
+                        <Grid item>
+                            <Typography
+                                variant="overline"
+                                color="textSecondary"
+                            >
+                                Generated {moment().format("LLLL")}
+                            </Typography>
                         </Grid>
                     </Grid>
-                </div>
-                <ReactToPrint
-                    trigger={this.getTrigger}
-                    content={this.getPrintContent}
-                />
+                    <Grid item container direction="column" spacing={24}>
+                        {classSchedules &&
+                            Array.from(MeetingDaysReadable).map(
+                                ([mdrEnum, mdrStr]) => {
+                                    const dayClasses = classSchedules!.filter(
+                                        cs => cs.meetingDays === mdrEnum
+                                    );
+
+                                    return (
+                                        <Grid item key={mdrEnum}>
+                                            <ScheduleTable
+                                                meetingDays={mdrStr}
+                                                classSchedules={dayClasses}
+                                            />
+                                        </Grid>
+                                    );
+                                }
+                            )}
+                    </Grid>
+                </Grid>
             </Paper>
         );
     }
