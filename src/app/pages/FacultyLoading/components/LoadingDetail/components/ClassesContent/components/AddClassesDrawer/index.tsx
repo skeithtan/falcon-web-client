10f5e3a61119ff6e0916c7a7df5c2c4694b0fd6c
hideyarguelles <catherine_arguelles@yahoo.com>
2019-03-02 14:28:37 +0800

    Add stepper for adding classes

--- src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/components/AddClassesDrawer/index.tsx
+++ src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/components/AddClassesDrawer/index.tsx
@@ -1,5 +1,10 @@
+import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import MenuItem from "@material-ui/core/MenuItem";
+import Step from "@material-ui/core/Step";
+import StepContent from "@material-ui/core/StepContent";
+import StepLabel from "@material-ui/core/StepLabel";
+import Stepper from "@material-ui/core/Stepper";
 import { withStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import { inject, observer } from "mobx-react";
@@ -17,9 +22,29 @@ interface IPropsType {
     classes: IStyleClasses;
 }
 
+interface IStateType {
+    activeStep: number;
+}
+
 @inject("facultyLoading")
 @observer
-class AddClassesDrawer extends React.Component<IPropsType> {
+class AddClassesDrawer extends React.Component<IPropsType, IStateType> {
+    public state = {
+        activeStep: 0,
+    };
+
+    public previousStep = () => {
+        this.setState({
+            activeStep: this.state.activeStep - 1,
+        });
+    };
+
+    public nextStep = () => {
+        this.setState({
+            activeStep: this.state.activeStep + 1,
+        });
+    };
+
     public onClose = () => {
         FacultyLoadingController.toggleAddClassesDrawer(false);
     };
@@ -47,6 +72,44 @@ class AddClassesDrawer extends React.Component<IPropsType> {
             classesTabState: { addClassesDrawerState, subjects },
         } = facultyLoading!;
         const { isShowing, form, validationErrors } = addClassesDrawerState;
+        const { activeStep } = this.state;
+
+        const stepLabels = ["Select a subject", "Add classes"];
+
+        const steps = [
+            <TextField
+                key="1"
+                select
+                label="Subject"
+                variant="outlined"
+                onChange={this.onSubjectChange}
+                value={form.subjectId || ""}
+                error={"subjectId" in validationErrors}
+                helperText={validationErrors.subjectId}
+                fullWidth
+            >
+                {subjects &&
+                    subjects.length > 0 &&
+                    subjects.map(s => (
+                        <MenuItem key={s.id} value={s.id}>
+                            {s.name}
+                        </MenuItem>
+                    ))}
+            </TextField>,
+            <Grid container direction="column" spacing={24} key="2">
+                <Grid item>
+                    <AddClassesTable onAddClick={this.onAddClick} />
+                </Grid>
+                <Grid item>
+                    <FormSubmitBar
+                        disabled={!form.subjectId || form.noClasses}
+                        formState={addClassesDrawerState}
+                        onSubmitClick={this.onSubmitClick}
+                    />
+                </Grid>
+            </Grid>,
+        ];
+
         return (
             <DrawerForm
                 open={isShowing}
@@ -54,43 +117,36 @@ class AddClassesDrawer extends React.Component<IPropsType> {
                 onClose={this.onClose}
                 className={classes.drawer}
             >
-                <Grid
-                    container
-                    direction="column"
-                    spacing={16}
-                    alignItems="stretch"
-                >
-                    <Grid item>
-                        <TextField
-                            select
-                            label="Subject"
-                            variant="outlined"
-                            onChange={this.onSubjectChange}
-                            value={form.subjectId || ""}
-                            error={"subjectId" in validationErrors}
-                            helperText={validationErrors.subjectId}
-                            fullWidth
-                        >
-                            {subjects &&
-                                subjects.length > 0 &&
-                                subjects.map(s => (
-                                    <MenuItem key={s.id} value={s.id}>
-                                        {s.name}
-                                    </MenuItem>
-                                ))}
-                        </TextField>
-                    </Grid>
-                    <Grid item>
-                        <AddClassesTable onAddClick={this.onAddClick} />
-                    </Grid>
-                    <Grid item>
-                        <FormSubmitBar
-                            disabled={!form.subjectId || form.noClasses}
-                            formState={addClassesDrawerState}
-                            onSubmitClick={this.onSubmitClick}
-                        />
-                    </Grid>
-                </Grid>
+                <Stepper activeStep={activeStep} orientation="vertical">
+                    {stepLabels.map((label, index) => (
+                        <Step key={index}>
+                            <StepLabel>{label}</StepLabel>
+                            <StepContent>
+                                <Grid container direction="column" spacing={16}>
+                                    <Grid item>{steps[index]}</Grid>
+                                    <Grid item>
+                                        <Button
+                                            disabled={index === 0}
+                                            onClick={this.previousStep}
+                                        >
+                                            Back
+                                        </Button>
+                                        {activeStep !== 1 && (
+                                            <Button
+                                                variant="contained"
+                                                color="primary"
+                                                onClick={this.nextStep}
+                                                disabled={!form.subjectId}
+                                            >
+                                                Proceed
+                                            </Button>
+                                        )}
+                                    </Grid>
+                                </Grid>
+                            </StepContent>
+                        </Step>
+                    ))}
+                </Stepper>
             </DrawerForm>
         );
     }
