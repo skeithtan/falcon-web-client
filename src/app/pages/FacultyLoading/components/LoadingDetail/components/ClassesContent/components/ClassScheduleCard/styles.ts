1028f852fc6bb706d56b41e2eb5572932ac324ca
hideyarguelles <catherine_arguelles@yahoo.com>
2019-03-13 23:18:21 +0800

    Add colour coding for rejected and adjunct classes

--- src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/components/ClassScheduleCard/styles.ts
+++ src/app/pages/FacultyLoading/components/LoadingDetail/components/ClassesContent/components/ClassScheduleCard/styles.ts
@@ -1,8 +1,18 @@
+import Grey from "@material-ui/core/colors/grey";
+import Red from "@material-ui/core/colors/red";
 import { StyleRules, Theme } from "@material-ui/core/styles";
 
 export default (theme: Theme): StyleRules => ({
     assigned: {
         background: theme.palette.primary.light,
-        color: theme.palette.common.white
+        color: theme.palette.common.white,
+    },
+    rejected: {
+        background: Red["200"],
+        color: theme.palette.common.white,
+    },
+    adjunct: {
+        background: Grey["500"],
+        color: theme.palette.common.white,
     },
 });
