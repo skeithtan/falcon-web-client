9579210eefdbb7c38cee28688429889ea4152258
hideyarguelles <catherine_arguelles@yahoo.com>
2019-03-16 16:54:07 +0800

    Add faculty sublist component

--- src/app/pages/FacultyLoading/components/LoadingDetail/components/FacultyContent/components/FacultyList/index.tsx
+++ src/app/pages/FacultyLoading/components/LoadingDetail/components/FacultyContent/components/FacultyList/index.tsx
@@ -1,11 +1,10 @@
 import List from "@material-ui/core/List";
-import ListSubheader from "@material-ui/core/ListSubheader";
 import { withStyles } from "@material-ui/core/styles";
 import { inject, observer } from "mobx-react";
 import * as React from "react";
 import IStyleClasses from "../../../../../../../../interfaces/style_classes";
 import { FacultyLoadingState } from "../../../../../../../../store/faculty_loading";
-import FacultyListItem from "./components/FacultyListItem";
+import FacultySublist from "./components/FacultySublist";
 import styles from "./styles";
 
 interface IPropsType {
@@ -25,19 +24,10 @@ class FacultyList extends React.Component<IPropsType> {
                 {Array.from(segmentedFacultyMembers.entries())
                     .filter(([sectionName, fms]) => fms.length > 0)
                     .map(([sectionName, fms]) => (
-                        <li key={sectionName} className={classes.listSection}>
-                            <ul className={classes.ul}>
-                                <ListSubheader className={classes.listHeader}>
-                                    {sectionName}
-                                </ListSubheader>
-                                {fms.map(fm => (
-                                    <FacultyListItem
-                                        key={fm.facultyId}
-                                        facultyMember={fm}
-                                    />
-                                ))}
-                            </ul>
-                        </li>
+                        <FacultySublist
+                            category={sectionName}
+                            facultyMembers={fms}
+                        />
                     ))}
             </List>
         );
