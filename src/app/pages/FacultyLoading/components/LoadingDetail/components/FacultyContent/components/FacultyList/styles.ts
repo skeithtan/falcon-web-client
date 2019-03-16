ef7737004c262595b9ec60011f3be0e800a6498e
hideyarguelles <catherine_arguelles@yahoo.com>
2019-03-16 16:53:30 +0800

    Style faculty list

--- src/app/pages/FacultyLoading/components/LoadingDetail/components/FacultyContent/components/FacultyList/styles.ts
+++ src/app/pages/FacultyLoading/components/LoadingDetail/components/FacultyContent/components/FacultyList/styles.ts
@@ -2,20 +2,10 @@ import Grey from "@material-ui/core/colors/grey";
 import { StyleRules, Theme } from "@material-ui/core/styles";
 
 export default (theme: Theme): StyleRules => ({
-    ul: {
-        backgroundColor: "inherit",
-        padding: 0,
-    },
-    listSection: {
-        backgroundColor: "inherit",
-    },
     list: {
-        background: theme.palette.background.paper,
+        background: Grey["100"],
         borderRight: `1px solid ${theme.palette.grey["200"]}`,
         height: "100%",
         overflowY: "scroll",
-    },
-    listHeader: {
-        backgroundColor: Grey["100"],
     },
 });
