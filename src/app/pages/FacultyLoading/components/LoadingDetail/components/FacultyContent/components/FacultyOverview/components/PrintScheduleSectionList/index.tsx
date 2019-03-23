72bb07dc256d9ab0764e027db3779e3f45d1f838
hideyarguelles <catherine_arguelles@yahoo.com>
2019-03-23 21:44:58 +0800

    Add observer and tuesday-friday map

--- src/app/pages/FacultyLoading/components/LoadingDetail/components/FacultyContent/components/FacultyOverview/components/PrintScheduleSectionList/index.tsx
+++ src/app/pages/FacultyLoading/components/LoadingDetail/components/FacultyContent/components/FacultyOverview/components/PrintScheduleSectionList/index.tsx
@@ -1,4 +1,5 @@
 import List from "@material-ui/core/List";
+import { observer } from "mobx-react";
 import * as React from "react";
 import DetailItem from "../../../../../../../../../../components/reusable/DetailItem";
 import FacultyClassSchedule from "../../../../../../../../../../models/entities/faculty_class_schedule";
@@ -9,16 +10,32 @@ interface IPropsType {
     classSchedules: FacultyClassSchedule[];
 }
 
+@observer
 export default class PrintScheduleSectionList extends React.Component<
     IPropsType
 > {
     public render() {
         const { classSchedules } = this.props;
+        console.log("rerender", classSchedules.length);
         return (
             <List>
                 {classSchedules.map(
                     cs =>
                         cs.meetingDays === MeetingDays.MondayThursday && (
+                            <DetailItem
+                                key={cs.id}
+                                field={
+                                    MeetingHoursReadable.get(cs.meetingHours)!
+                                }
+                                value={`${cs.subject.name} ${cs.section} ${
+                                    cs.room
+                                }`}
+                            />
+                        )
+                )}
+                {classSchedules.map(
+                    cs =>
+                        cs.meetingDays === MeetingDays.TuesdayFriday && (
                             <DetailItem
                                 key={cs.id}
                                 field={
